14cdde617cf84a214065255888b5fbabff8d0474
Matthew Dick <mattwd7@gmail.com>
2012-09-15 11:52:59 -0700

    DB Population

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+File.open("db_fields.txt", "r") do |infile|
+	while (line = infile.gets)
+		Field.create(:title => line)
+	end
+end
+
+File.open("db_courses.txt", "r") do |infile|
+	every_three = 3
+	while (line = infile.gets)
+		if every_three < 3
+			every_three = every_three + 1
+		else
+			every_three = 1
+			f_id = line
+			next
+		end
+		if every_three == 3
+			next
+		end
+
+		Course.create(:field_id => f_id, :title => line)
+
+	end
+end
