a36939296944e8b33e17c9d744056350fc283dc3
Matthew Dick <mattwd7@gmail.com>
2012-09-19 14:14:00 -0700

    mod prod.rb initializer and added setup_mail.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Hhreboot2::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'hillheroes.com' }
+  config.action_mailer.default_url_options = { :host => 'hh001.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
