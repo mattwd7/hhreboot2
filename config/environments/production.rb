06ad2ee52a4c6e3edc5f29a876b914f30f23f126
Matthew Dick <mattwd7@gmail.com>
2012-09-19 14:00:27 -0700

    mod prod.rb initializer and added setup_mail.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,4 +66,8 @@ Hhreboot2::Application.configure do
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
   config.action_mailer.default_url_options = { :host => 'hillheroes.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 end
