5eecb28a05c0cf0db524fa4d45e8385be37fb9df
Matthew Dick <mattwd7@gmail.com>
2012-09-22 19:28:08 -0700

    anonymous posting

---
+++ app/decorators/controllers/forem/posts_controller_decorator.rb
@@ -0,0 +1,23 @@
+Forem::PostsController.class_eval do
+    def create
+      authorize! :reply, @topic
+      if @topic.locked?
+        flash.alert = t("forem.post.not_created_topic_locked")
+        redirect_to [@topic.forum, @topic] and return
+      end
+      @post = @topic.posts.build(params[:post])
+      if params[:anonymous_post]
+      	@post.user = User.where(:username => "Anonymous").first
+      else
+      	@post.user = forem_user
+      end
+      if @post.save
+        flash[:notice] = t("forem.post.created")
+        redirect_to forum_topic_url(@topic.forum, @topic, :page => last_page)
+      else
+        params[:reply_to_id] = params[:post][:reply_to_id]
+        flash.now.alert = t("forem.post.not_created")
+        render :action => "new"
+      end
+    end
+end
