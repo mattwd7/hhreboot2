766568dbd675932d794816258eb569f10059a1ca
Matthew Dick <mattwd7@gmail.com>
2012-09-25 12:14:20 -0700

    fixed JS bug

--- app/assets/javascripts/test_bank.js
+++ app/assets/javascripts/test_bank.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
 	$(".upvote").click(function(){
-		$(this).attr("src", "/upvote.png");
 		var quality = $(this).parent().text()
 		var new_quality = parseInt(quality) + 1;
 		new_quality = (new_quality > 0 ? "+" + new_quality : new_quality)
 		$(this).parent().next().show()
-		$(this).parent().html($("<img src='/upvote.png' /> " + new_quality + " <img src='/downvote_neutral.png' />"))
+		$(this).parent().html($("<img src='/upvote.png' > " + new_quality + " <img src='/downvote_neutral.png' >"))
 	})
 	$(".downvote").click(function(){
-		$(this).attr("src", "/downvote.png");
 		var quality = $(this).parent().text()
 		var new_quality = parseInt(quality) - 1;
 		new_quality = (new_quality > 0 ? "+" + new_quality : new_quality)
 		$(this).parent().next().next().show()
-		$(this).parent().html($("<img src='/upvote_neutral.png' /> " + new_quality + " <img src='/downvote.png' />"))
+		$(this).parent().html($("<img src='/upvote_neutral.png' > " + new_quality + " <img src='/downvote.png' >"))
 	})
 })
