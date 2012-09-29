cb6c6696f65b975aa4b1bb85d5ae1fb75620b58a
Matthew Dick <mattwd7@gmail.com>
2012-09-28 21:48:31 -0700

    new homepage look

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -2,42 +2,7 @@ $(document).ready(function() {
   var leftWidth = $(".left").width();
   var rightWidth = $(".right").width();
   var totalWidth = leftWidth + rightWidth + 30;
-  $("#navigation").css("width", (totalWidth) + 'px');
-
-  $(function() {
-    $( "#accordion" ).accordion({
-      collapsible: false
-    });
-  });
-
-  function cycleSections(){
-    var next = $(".cyclotron").nextAll("h3");
-    $(".cyclotron").removeClass("cyclotron");
-    if (next.length ==0){
-      next = $("#top")
-    }
-    next.addClass("cyclotron");
-    next.click();
-    if ($(document).mousemove()){
-      return false;
-    }
-    else{
-      setTimeout(cycleSections, 3000)
-    }
-
-  };
-
-  var timer;
-  $(document).mousemove(function() {
-      if (timer) {
-          clearTimeout(timer);
-          timer = 0;
-      }
-
-      timer = setTimeout(function(){
-          cycleSections();
-        }, 10000)
-  });
+  //$("#navigation").css("width", (totalWidth) + 'px');
 
   $("#navigation a").hover(function(){
     $(this).animate({
