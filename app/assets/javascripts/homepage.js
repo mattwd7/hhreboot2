3bd183ab09a192cddb488a0613ce3647a7e93227
Matthew Dick <mattwd7@gmail.com>
2012-09-30 21:02:35 -0700

    added resources to homepage

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -27,6 +27,18 @@ $(document).ready(function() {
     })
   })  
 
+  $("#resources_link").click(function(){
+    $("#resources_panel").show("blind");
+  })
+
+  $(".close_resources").mouseleave(function(){
+    if($("#resources_panel").css("display") != "none"){
+      //if(!$("#resources_panel").mouseover() && !$("#navigation").mouseover()){
+         $("#resources_panel").hide("blind");
+      //}
+    }
+  })
+
   /*$("#dining_menu").click(function(){
     $("#dining_menu").siblings().fadeTo("slow", "1.0")
     $("#dining_menu").fadeTo("slow", "0", function(){
