170f775c3086c9da45cbeb195c9b0a3e6ce91716
Matthew Dick <mattwd7@gmail.com>
2012-10-02 21:56:14 -0700

    homepage redo

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -40,6 +40,51 @@ $(document).ready(function() {
     }
   })
 
+  $(function() {
+    $( "#accordion" ).accordion({
+      collapsible: false
+    });
+  });
+
+  function cycleSections(){
+    var next = $(".cyclotron").nextAll("h3");
+    $(".cyclotron").removeClass("cyclotron");
+    if (next.length ==0){
+      next = $("#top")
+    }
+    next.addClass("cyclotron");
+    next.click();
+    if ($(document).mousemove()){
+      return false;
+    }
+    else{
+      setTimeout(cycleSections, 3000)
+    }
+
+  };
+
+  var timer;
+  $(document).mousemove(function() {
+      if (timer) {
+          clearTimeout(timer);
+          timer = 0;
+      }
+
+      timer = setTimeout(function(){
+          cycleSections();
+        }, 10000)
+  });
+
+  $("#navigation a").hover(function(){
+    $(this).animate({
+      color: "white"
+    }, 300)
+  }, function(){
+    $(this).animate({
+      color: "black"
+    }, 300)
+  })
+
   /*$("#dining_menu").click(function(){
     $("#dining_menu").siblings().fadeTo("slow", "1.0")
     $("#dining_menu").fadeTo("slow", "0", function(){
